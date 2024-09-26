8938f8bebbb5afe183aff4275cfa799e0242e313
jin214930 <jin214930@naver.com>
2024-09-26 19:07:10 +0900

    .gitignore update

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,7 +4,25 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
+		int[] a = new int[4];
+		int[] cnt = new int[100];
+		int ans = 0;
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i < 4; i++)
+			a[i] = Integer.parseInt(st.nextToken());
+
+		for (int i = 0; i < 3; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			for (int j = s; j < e; j++)
+				cnt[j]++;
+		}
+
+		for (int i = 0; i < 100; i++)
+			ans += cnt[i] * a[cnt[i]];
+
+		System.out.println(ans);
 	}
 }
